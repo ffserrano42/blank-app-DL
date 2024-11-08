13f98cb049ffbed80417378bd13b79d35be80db1
ffserrano42 <ff.serrano42@uniandes.edu.co>
2024-11-08 18:45:21 -0500

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,15 @@ from ultralytics import YOLO
 import numpy as np
 
 # Cargar modelo YOLO
-model = YOLO("yolov8n.pt") 
+model = YOLO("yolov8n.pt")
+model_proyecto=YOLO("best_proyecto_v11.pt")
 
 # Título de la aplicación
-st.title("Aplicación de reconocimiento de imágenes con YOLO V 1.0 ")
-st.text("Cargue una imagen y presione el botón de 'Procesar' para detectar objetos en la imagen. Solo se mostraran aquellos cuya probabilidad sea mayor a 0.65")
+st.title("Aplicación de reconocimiento de imágenes de plantas de papa con YOLO ")
+st.text("seleccione una imagen y presione el botón de 'Procesar' para detectar las plantas de papa ")
+
+#slider para manejar la confianza
+score_threshold = st.slider("Seleccione el score mínimo de confianza para detección", 0.0, 1.0, 0.3, 0.1)
 
 # Carga de la imagen
 uploaded_image = st.file_uploader("Cargar imagen", type=["jpg", "png", "jpeg"])
@@ -22,48 +26,49 @@ uploaded_image = st.file_uploader("Cargar imagen", type=["jpg", "png", "jpeg"])
 if uploaded_image:
     # Mostrar la imagen cargada
     image = Image.open(uploaded_image)
-    st.image(image, caption="Imagen cargada", use_column_width=True)
-    
+    #st.image(image, caption="Imagen cargada", use_column_width=True)
+    col1,col2=st.columns(2)
+    with col1:
+        st.image(image, caption="Imagen cargada", use_column_width=True)
     # Botón de "Procesar"
     if st.button("Procesar"):
         # Barra de progreso
         progress_bar = st.progress(0)
         st.text("procesando")
-        object_names = list(model.names.values())
         #st.text(object_names)
         
         # Simulación de proceso con la barra de progreso
         for percent_complete in range(100):
             time.sleep(0.01)
-            progress_bar.progress(percent_complete + 1)
-        
+            progress_bar.progress(percent_complete + 1)        
         # Procesar la imagen con el modelo YOLO
-        original_image=np.array(image)
-        result = model(image)  # Usar predict para evaluar la imagen    
-        result_predict=model.predict(image)
-        class_counts = {}
-        for detection in result[0].boxes.data:
+        original_image_v11=np.array(image)
+        result_v11=model_proyecto(image)    
+        result_predict_v11=model_proyecto.predict(image)     
+        st.subheader('Conteo de clases detectadas con deteccion v11')        
+        class_counts_v11 = {}
+        for detection in result_v11[0].boxes.data:
                     x0, y0 = (int(detection[0]), int(detection[1]))
                     x1, y1 = (int(detection[2]), int(detection[3]))
                     score = round(float(detection[4]), 2)
                     cls = int(detection[5])
-                    object_name =  model.names[cls]                    
+                    object_name =  model_proyecto.names[cls]                    
                     # Dibuja la caja en la imagen y tambien la probabilidad
-                    if(score>0.65):
+                    if(score>=score_threshold):
                         # Contar las ocurrencias de cada clase para mostrarlas al final
-                        if object_name in class_counts:
-                            class_counts[object_name] += 1
+                        if object_name in class_counts_v11:
+                            class_counts_v11[object_name] += 1
                         else:
-                            class_counts[object_name] = 1
-                        cv2.rectangle(original_image, (x0, y0), (x1, y1), (0, 255, 0), 4)                    
+                            class_counts_v11[object_name] = 1
+                        cv2.rectangle(original_image_v11, (x0, y0), (x1, y1), (255, 0,0), 1)                    
                         # Añade la etiqueta y la probabilidad a la caja
                         label = f'{object_name}: {score}'
-                        cv2.putText(original_image, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)                                          
-                    
-        st.image(original_image, caption="Imagen con detecciones", use_column_width=True)
-        st.subheader('Conteo de clases detectadas')
+                        cv2.putText(original_image_v11, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0,0), 1)                                         
+        print(class_counts_v11)
+        with col2:
+            st.image(original_image_v11, caption="Imagen con detecciones Yolo V11 entrenado", use_column_width=True)      
+        class_count_df_11 = pd.DataFrame(class_counts_v11.items(), columns=['Clase', 'Ocurrencias'])
+        st.table(class_count_df_11)
 
-        class_count_df = pd.DataFrame(class_counts.items(), columns=['Clase', 'Ocurrencias'])
-        st.table(class_count_df)
 
 
