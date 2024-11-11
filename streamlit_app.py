1533e2a6c04c7803e9d5204b9e90694e0e874040
ffserrano42 <ff.serrano42@uniandes.edu.co>
2024-11-11 12:22:53 -0500

    adicion de columnas para comparar modelo con y sin aumentaciones

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ import numpy as np
 
 # Cargar modelo YOLO
 model = YOLO("yolov8n.pt")
-model_proyecto=YOLO("best_proyecto_v11.pt")
+model_proyecto_aug=YOLO("best_proyecto_v11_5.pt") # este es el modelo entrenado con datos aumentados
+model_proyecto=YOLO("best_proyecto_v11.pt")  #este es el modelo entrenado sin datos aumentados.
 
 # Título de la aplicación
 st.title("Aplicación de reconocimiento de imágenes de plantas de papa con YOLO ")
@@ -27,7 +28,7 @@ if uploaded_image:
     # Mostrar la imagen cargada
     image = Image.open(uploaded_image)
     #st.image(image, caption="Imagen cargada", use_column_width=True)
-    col1,col2=st.columns(2)
+    col1,col2,col3=st.columns(3)
     with col1:
         st.image(image, caption="Imagen cargada", use_container_width=True)
     # Botón de "Procesar"
@@ -43,8 +44,9 @@ if uploaded_image:
             progress_bar.progress(percent_complete + 1)        
         # Procesar la imagen con el modelo YOLO
         original_image_v11=np.array(image)
+        original_image_v11_aug=np.array(image)
         result_v11=model_proyecto(image)    
-        result_predict_v11=model_proyecto.predict(image)     
+        result_v11_aug=model_proyecto_aug(image)           
         st.subheader('Conteo de clases detectadas con deteccion v11')        
         class_counts_v11 = {}
         for detection in result_v11[0].boxes.data:
@@ -66,9 +68,32 @@ if uploaded_image:
                         cv2.putText(original_image_v11, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0,0), 1)                                         
         print(class_counts_v11)
         with col2:
-            st.image(original_image_v11, caption="Imagen con detecciones Yolo V11 entrenado", use_container_width=True)      
-        class_count_df_11 = pd.DataFrame(class_counts_v11.items(), columns=['Clase', 'Ocurrencias'])
-        st.table(class_count_df_11)
+            st.image(original_image_v11, caption="Imagen con detecciones sin aumentacion", use_container_width=True)      
+            class_count_df_11 = pd.DataFrame(class_counts_v11.items(), columns=['Clase', 'Ocurrencias'])
+            st.table(class_count_df_11)
+        class_counts_v11_aug = {}
+        for detection in result_v11_aug[0].boxes.data:
+                    x0, y0 = (int(detection[0]), int(detection[1]))
+                    x1, y1 = (int(detection[2]), int(detection[3]))
+                    score = round(float(detection[4]), 2)
+                    cls = int(detection[5])
+                    object_name =  model_proyecto_aug.names[cls]                    
+                    # Dibuja la caja en la imagen y tambien la probabilidad
+                    if(score>=score_threshold):
+                        # Contar las ocurrencias de cada clase para mostrarlas al final
+                        if object_name in class_counts_v11_aug:
+                            class_counts_v11_aug[object_name] += 1
+                        else:
+                            class_counts_v11_aug[object_name] = 1
+                        cv2.rectangle(original_image_v11_aug, (x0, y0), (x1, y1), (255, 0,0), 1)                    
+                        # Añade la etiqueta y la probabilidad a la caja
+                        label = f'{object_name}: {score}'
+                        cv2.putText(original_image_v11_aug, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0,0), 1)                                         
+        print(class_counts_v11_aug)        
+        with col3:
+            st.image(original_image_v11_aug, caption="Imagen con detecciones con aumentacion", use_container_width=True)      
+            class_count_df_11_aug = pd.DataFrame(class_counts_v11_aug.items(), columns=['Clase', 'Ocurrencias'])
+            st.table(class_count_df_11_aug)
 
 
 
