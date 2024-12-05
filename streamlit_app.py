f6ea05487e105fccef52ab98d4c8f9a003daba27
ffserrano42 <ff.serrano42@uniandes.edu.co>
2024-12-05 00:35:25 -0500

    cargue de la aplicacion

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,30 @@
 import cv2
+import psutil
 import streamlit as st
 from PIL import Image
 import time
 import pandas as pd
 from io import BytesIO
-import ultralytics 
 from ultralytics import YOLO
 import numpy as np
+
+print(f"Uso de memoria al iniciar la aplicacion RAM: {psutil.virtual_memory().used / 1e6} MB")
+# Cargar modelo YOLO
+
+@st.cache_resource
+def load_model_sa():
+     print("cargando modelo sin aumentacion")
+     return YOLO("best_model_sa_1.pt")
+
+@st.cache_resource
+def load_model_ca():
+    print("cargando modelo con aumentacion")
+    return YOLO("best_proyecto_conaumentacion.pt")
+
+
+# Cargar modelo YOLO
+#model_proyecto=load_model_sa()  #este es el modelo entrenado sin datos aumentados.
+#model_proyecto_aug=load_model_ca() # este es el modelo entrenado con datos aumentados
 
 
 
@@ -62,65 +80,60 @@ if uploaded_image:
         progress_bar = st.progress(0)
         st.text("procesando")
         #st.text(object_names)
-        
+        model_proyecto=load_model_sa()  #este es el modelo entrenado sin datos aumentados.        
+        model_proyecto_aug=load_model_ca() # este es el modelo entrenado con datos aumentados       
         # Simulación de proceso con la barra de progreso
         for percent_complete in range(100):
             time.sleep(0.01)
             progress_bar.progress(percent_complete + 1)        
+        # Procesar la imagen con el modelo YOLO
         original_image_v11=np.array(image)
         original_image_v11_aug=np.array(image)
-        # Cargue de modelo sin aumentacion se debe comentarear ya que streamlit cloud esta teniendo inconvenientes con la carga de modelos asi que solo se puede cargar un modelo :'(      
-        #model_proyecto=YOLO("best_model_sa_1.pt")  #este es el modelo entrenado sin datos aumentados. 
-        #result_v11=model_proyecto.predict(image,max_det=1500)#se coloca el maximo de detecciones posibles
-          
-        # Cargue de modelo con aumentacion
-        model_proyecto_aug=YOLO("best_proyecto_conaumentacion.pt") # este es el modelo entrenado con datos aumentados                       
-        result_v11_aug=model_proyecto_aug.predict(image,max_det=1500)#se coloca el maximo de detecciones posibles o de lo contrario maximo muestra 300 detecciones                   
-        ###Se comentarea la principal funcionalidad del modelo sin validaciones para que no genere inconvenientes en la aplicaion de streamlit cloud, sinembargo si corre en local
-        ### Este mensaje de error solo salio desde el 2 de Diciembre de 2024, ya que durante la sustentacion se corrio sin problemas en streamlit cloud
+        result_v11=model_proyecto.predict(image,max_det=1500)#se coloca el maximo de detecciones posibles         
+        result_v11_aug=model_proyecto_aug.predict(image,max_det=1500)#se coloca el maximo de detecciones posibles                   
         #st.subheader('Conteo de clases detectadas con deteccion v11')        
         class_counts_v11 = {}
-        #for detection in result_v11[0].boxes.data:
-        #            x0, y0 = (int(detection[0]), int(detection[1]))
-        #            x1, y1 = (int(detection[2]), int(detection[3]))
-        #            score = round(float(detection[4]), 2)
-        #            cls = int(detection[5])
-        #            object_name =  model_proyecto.names[cls]                                                        
-        #            # Agrupa las detecciones por rango de score
-        #            if score < 0.1:
-        #                detections_by_score_range[f"0%-10%"] += 1
-        #            elif score < 0.2:
-        #                detections_by_score_range[f"10%-20%"] += 1
-        #            elif score < 0.3:
-        #                detections_by_score_range[f"20%-30%"] += 1
-        #            elif score < 0.4:
-        #                detections_by_score_range[f"30%-40%"] += 1
-        #            elif score < 0.5:
-        #                detections_by_score_range[f"40%-50%"] += 1
-        #            elif score < 0.6:
-        #                detections_by_score_range[f"50%-60%"] += 1
-        #            elif score < 0.7:
-        #                detections_by_score_range[f"60%-70%"] += 1
-        #            elif score < 0.8:
-        #                detections_by_score_range[f"70%-80%"] += 1
-        #            elif score < 0.9:
-        #                detections_by_score_range[f"80%-90%"] += 1
-        #            else:
-        #                detections_by_score_range[f"90%-100%"] += 1
-        #            # Dibuja la caja en la imagen y tambien la probabilidad
-        #            if(score>=score_threshold):                        
-        #                # Contar las ocurrencias de cada clase para mostrarlas al final
-        #                if object_name in class_counts_v11:
-        #                    class_counts_v11[object_name] += 1
-        #                    print(class_counts_v11[object_name])
-        #                else:
-        #                    class_counts_v11[object_name] = 1
-        #                cv2.rectangle(original_image_v11, (x0, y0), (x1, y1), (255, 0,0), 1)                    
-        #                # Añade la etiqueta y la probabilidad a la caja
-        #                label = f'{object_name}: {score}'
-        #                cv2.putText(original_image_v11, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0,0), 1)                                               
+        for detection in result_v11[0].boxes.data:
+                    x0, y0 = (int(detection[0]), int(detection[1]))
+                    x1, y1 = (int(detection[2]), int(detection[3]))
+                    score = round(float(detection[4]), 2)
+                    cls = int(detection[5])
+                    object_name =  model_proyecto.names[cls]                                                        
+                    # Agrupa las detecciones por rango de score
+                    if score < 0.1:
+                        detections_by_score_range[f"0%-10%"] += 1
+                    elif score < 0.2:
+                        detections_by_score_range[f"10%-20%"] += 1
+                    elif score < 0.3:
+                        detections_by_score_range[f"20%-30%"] += 1
+                    elif score < 0.4:
+                        detections_by_score_range[f"30%-40%"] += 1
+                    elif score < 0.5:
+                        detections_by_score_range[f"40%-50%"] += 1
+                    elif score < 0.6:
+                        detections_by_score_range[f"50%-60%"] += 1
+                    elif score < 0.7:
+                        detections_by_score_range[f"60%-70%"] += 1
+                    elif score < 0.8:
+                        detections_by_score_range[f"70%-80%"] += 1
+                    elif score < 0.9:
+                        detections_by_score_range[f"80%-90%"] += 1
+                    else:
+                        detections_by_score_range[f"90%-100%"] += 1
+                    # Dibuja la caja en la imagen y tambien la probabilidad
+                    if(score>=score_threshold):                        
+                        # Contar las ocurrencias de cada clase para mostrarlas al final
+                        if object_name in class_counts_v11:
+                            class_counts_v11[object_name] += 1
+                            print(class_counts_v11[object_name])
+                        else:
+                            class_counts_v11[object_name] = 1
+                        cv2.rectangle(original_image_v11, (x0, y0), (x1, y1), (255, 0,0), 1)                    
+                        # Añade la etiqueta y la probabilidad a la caja
+                        label = f'{object_name}: {score}'
+                        cv2.putText(original_image_v11, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0,0), 1)                                               
         with col2:
-            st.image(original_image_v11, caption="Imagen con detecciones sin aumentacion - modelo no disponible-", use_container_width=True)      
+            st.image(original_image_v11, caption="Imagen con detecciones sin aumentacion", use_container_width=True)      
             # Convertir el diccionario de conteo a un DataFrame
             class_count_df_11 = pd.DataFrame(class_counts_v11.items(), columns=['Clase', 'Ocurrencias'])          
             df_score_range = pd.DataFrame(list(detections_by_score_range.items()), columns=['Score Range', 'Total Detections'])
@@ -167,7 +180,7 @@ if uploaded_image:
                         cv2.rectangle(original_image_v11_aug, (x0, y0), (x1, y1), (255, 0,0), 1)                    
                         # Añade la etiqueta y la probabilidad a la caja
                         label = f'{object_name}: {score}'
-                        cv2.putText(original_image_v11_aug, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0,0), 1)                                                 
+                        cv2.putText(original_image_v11_aug, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0,0), 1)        
         with col3:
             st.image(original_image_v11_aug, caption="Imagen con detecciones con aumentacion", use_container_width=True)      
             class_count_df_11_aug = pd.DataFrame(class_counts_v11_aug.items(), columns=['Clase', 'Ocurrencias'])
@@ -175,7 +188,4 @@ if uploaded_image:
             st.table(class_count_df_11_aug)
             st.subheader('total by score')
             st.table(df_score_range_aug)
-            
-
-
-
+print(f"Uso de memoria al finalizar la aplicacion RAM: {psutil.virtual_memory().used / 1e6} MB")
