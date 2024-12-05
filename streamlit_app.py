c853e0b36f865736945d2d4b5dfa3d0321f84252
ffserrano42 <ff.serrano42@uniandes.edu.co>
2024-12-04 19:37:42 -0500

    modificacion a requeriments

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,20 @@ import ultralytics
 from ultralytics import YOLO
 import numpy as np
 
+
+@st.cache_data
+def load_model_sa():
+	  return YOLO("best_model_sa_1.pt")
+
+@st.cache_data
+def load_model_ca():
+	  return YOLO("best_proyecto_conaumentacion.pt")
+
+
 # Cargar modelo YOLO
-model_proyecto=YOLO("best_model_sa_1.pt")  #este es el modelo entrenado sin datos aumentados.
+model_proyecto=load_model_sa()  #este es el modelo entrenado sin datos aumentados.
 #model_proyecto_aug=YOLO("best_proyecto_v11_6.pt") # este es el modelo entrenado con datos aumentados
-model_proyecto_aug=YOLO("best_proyecto_conaumentacion.pt") # este es el modelo entrenado con datos aumentados
+model_proyecto_aug=load_model_ca() # este es el modelo entrenado con datos aumentados
 
 #rango de detecciones por score para que en cada modelo se pueda mostrar las detecciones por score
 detections_by_score_range = {
